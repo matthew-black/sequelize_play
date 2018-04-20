cd48243aafd36ff0f6adad43e14edb121465f87d
Matthew Black <matthewallenblack@gmail.com>
2018-04-19 20:30:02 -0500

    Since we're playing with new things, added eslint.

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
+
 const hostname = '127.0.0.1';
 const port = 3030;
 
+app.use(morgan('dev'));
+
 app.get('/', (req, res) => {
   res.send('Hello Self!');
-})
+});
 
 app.listen(port, hostname, () => {
-  console.log('I guess you should check out http://localhost:' + port);
-})
+  console.log(`I guess you should check out http://localhost:${port}`);
+});
