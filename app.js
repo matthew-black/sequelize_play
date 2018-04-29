3c107a16fbd9dda0a7d5afcd0540075304120a96
Matthew Black <matthewallenblack@gmail.com>
2018-04-28 21:08:04 -0500

    Make and link to a show page for each book.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
-const db = require('./src/models')
+const models = require('./src/models')
 
 const app = express();
 
@@ -16,19 +16,32 @@ app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 app.get('/', (req, res) => {
-  let books;
-  db.Book.findAll({
-    include: [db.Comment]
+  models.Book.findAll({
+    include: [models.Comment]
   }).then((books) => {
     // res.json(books)
     res.render("index", {books: books})
   })
-  
-});
+})
+
+app.get('/books/:id', (req, res) => {
+  models.Book.findOne({
+    where: {id: req.params.id},
+    include: [{
+      model: models.Comment,
+      where: { book_id: req.params.id }
+    }]
+  }).then((book) => {
+    console.log("************************************")
+    console.log(book.Comments)
+    console.log("************************************")
+    res.render("show", {book: book})
+  })
+})
 
 app.get('/favicon.ico', (req, res) => {
   res.status(204);
-});
+})
 
 app.listen(port, hostname, () => {
   console.log(`I guess you should check out http://localhost:${port}`);
